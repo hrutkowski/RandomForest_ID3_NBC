40fd30510c9b3940922e68caf8dae23ab3938cbd
hrutkowski <115936497+hrutkowski@users.noreply.github.com>
2024-01-20 19:06:46 +0100

    ready Random Forest

--- algorithms/random_forest_algorithm.py
+++ algorithms/random_forest_algorithm.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from typing import List, Tuple
-from sklearn import metrics
+from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
 from .id3_classifier_old import ID3
 from .nbc_classifier import NBC
 
@@ -9,31 +9,22 @@ from .nbc_classifier import NBC
 class RandomForest:
 
     def __init__(self, n: int = 100, samples_percentage: float = 0.5, attributes_percentage: float = 0.5,
-                 classifier_list: List = [ID3, NBC], classifier_ratio_list: List = [0.5, 0.5]):
+                 classifiers: List = [ID3, NBC], classifiers_ratios: List = [0.5, 0.5]):
+        self.attributes = []
+        self.forest = []
         self.n = n
         self.samples_percentage = samples_percentage
         self.attributes_percentage = attributes_percentage
-        self.classifier_list = classifier_list
-        self.classifier_ratio_list = classifier_ratio_list
-        self.forest = []
-        self.attributes = []
+        self.classifiers = classifiers
+        self.classifiers_ratios = classifiers_ratios
 
     def fit(self, X_train, y_train):
-        assert len(self.classifier_list) == len(self.classifier_ratio_list), \
-            "List of classifiers and classifier ratios must have the same size!"
-        for classifier, ratio in zip(self.classifier_list, self.classifier_ratio_list):
+        for classifier, ratio in zip(self.classifiers, self.classifiers_ratios):
             for _ in range(round(self.n * ratio)):
                 clf = classifier()
-                X_train_bagging, y_train_bagging = self.bagging_data(X_train.copy(), y_train.copy())
-                clf.fit(X_train_bagging.copy(), y_train_bagging.copy())
+                X_train_bag, y_train_bag = self._bagging(X_train, y_train)
+                clf.fit(X_train_bag, y_train_bag)
                 self.forest.append(clf)
-
-    def bagging_data(self, X_train, y_train):
-        X_train = X_train.sample(frac=self.attributes_percentage, axis=1)
-        self.attributes.append(X_train.columns)
-        X_train['results'] = y_train
-        X_train = X_train.sample(frac=self.percent_samples)
-        return X_train.drop(columns=['results']), X_train['results']
 
     def predict(self, X_test) -> pd.Series:
         predictions = np.empty([len(self.forest), len(X_test)])
@@ -43,13 +34,15 @@ class RandomForest:
         pred_df = pd.DataFrame.mode(pred_df, axis=0).T
         return pd.Series(pred_df[0])
 
-    def get_scores(self, X_test, y_test) -> Tuple[float, float]:
-        y_pred = np.array(self.predict(X_test))
-        y_test = np.array(y_test)
-        return metrics.accuracy_score(y_test, y_pred), metrics.f1_score(y_test, y_pred, average='macro')
+    def eval(self, X_test, y_test) -> Tuple[float, float, float]:
+        y_test_np = np.array(y_test)
+        y_pred_np = np.array(self.predict(X_test))
+        return (accuracy_score(y_test_np, y_pred_np), f1_score(y_test_np, y_pred_np, average='macro'),
+                confusion_matrix(y_test_np, y_pred_np))
 
-    def eval(self, X_test, y_test) -> Tuple[float, float]:
-        accuracy, f1_score = self.get_scores(X_test, y_test)
-        print(f"Accuracy: {accuracy}")
-        print(f"F1 score: {f1_score}")
-        return accuracy, f1_score
+    def _bagging(self, X_train, y_train):
+        bagging_data = X_train.sample(frac=self.attributes_percentage, axis=1)
+        self.attributes.append(bagging_data.columns)
+        bagging_data['y'] = y_train
+        bagging_data = bagging_data.sample(frac=self.samples_percentage)
+        return bagging_data.drop(columns=['y']), bagging_data['y']
