09920e94139d79046dac9012b519e2ca77ad6b0e
szumadame <szumada.a@gmail.com>
2024-01-26 21:00:33 +0100

    last check

--- code/main.py
+++ code/main.py
@@ -2,12 +2,7 @@ from scripts_and_experiments.do_experiments import id3_comparison, nbc_compariso
     classifier_ratio_influence, samples_percentage_influence
 
 nbc_comparison('loan_approval')
-nbc_comparison('corona')
 id3_comparison('loan_approval')
-id3_comparison('corona')
 tree_number_influence('glass')
-tree_number_influence('letter')
 samples_percentage_influence('glass')
-samples_percentage_influence('corona')
-classifier_ratio_influence('loan_approval')
 classifier_ratio_influence('letter')
